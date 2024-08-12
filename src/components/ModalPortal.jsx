ce56de2406a2ebbae05ed31cda50e54c754ea52b
Agus Baez <agustinbaezignacio@gmail.com>
2024-08-12 20:57:49 -0300

    Portals

--- src/components/ModalPortal.jsx
+++ src/components/ModalPortal.jsx
@@ -1,11 +1,25 @@
-export const ModalPortal = () => {
-  return (
-    <article> 
-      <div>
-        <button>X</button>
+import styles from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+export default function ModalPortal({ children, isOpen, closeModal }) {
+  const handleClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return ReactDOM.createPortal(
+    <article
+      className={`${styles.modal} ${isOpen && styles.isOpen}`}
+      onClick={closeModal}
+    >
+      <div className={styles.modalContent} onClick={handleClick}>
+        <div>I`m a modal dialog</div>
+        <button className={styles.modalClose} onClick={closeModal}>
+          X
+        </button>
         {children}
       </div>
-    </article>
+    </article>,
+    document.getElementById("modal")
   );
-};
+}
 
--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,8 +1,10 @@
 import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
+import ModalPortal from "./ModalPortal";
 
 export const Modals = () => {
   const [isOpenModal, openModal, closeModal] = useModal(false);
+  const [isOpenPortal, openModalPortal, closeModalPortal] = useModal(false);
   return (
     <div>
       <h2>Modals</h2>
@@ -17,6 +19,21 @@ export const Modals = () => {
         </p>
         <img src="https://i.imgur.com/fClJeO6.jpg" alt="Animals" />
       </Modal>
+
+      <button onClick={openModalPortal}>Modal Portal</button>
+      <ModalPortal isOpen={isOpenPortal} closeModal={closeModalPortal}>
+        <h3>Modal Portal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          sapiente tempora cumque eaque quae numquam, aliquid exercitationem
+          aspernatur sed, laborum quas earum dolore voluptatem. Unde molestias
+          nostrum error veniam cum!
+        </p>
+        <img
+          src="https://img.freepik.com/vetores-gratis/conjunto-de-raios-de-brilho-redondo-de-neon_1441-4099.jpg?t=st=1723506381~exp=1723506981~hmac=6f5986c9b864c389135d62255dfc195df74701428675304718ab39f823817dc3"
+          alt="Portals"
+        />
+      </ModalPortal>
     </div>
   );
 };
